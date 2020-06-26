fa250681d07dd4e3f55d9640750fc9eb1b9605a7
HyperionSuperNova <n.amari.pro@gmail.com>
2020-06-26 16:10:28 +0200

    Moving to scripts

--- Scripts/link_dynamics.py
+++ Scripts/link_dynamics.py
@@ -6,77 +6,60 @@ import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 import dask.dataframe as dd
+from alters_count import get_ego_id, add_value_to_dict
+
+egos_recent_alter_count_path = '../Alter-count-csv-plots/*/csv/*'
+egos_sociocat_path = '../sample_data_egos_age/egos-age-gender-profession.csv'
 
 
-def get_ego_id(ego_path):
-    return os.path.splitext(os.path.basename(ego_path))[0][:8]
-
-
-def add_value_to_dict(dico, key, val):
-    if key in dico.keys():
-        dico[key] = dico[key] + val
-    else:
-        dico[key] = val
-
-
-def mean_recent_alter_ego(csvobj):
-    len_recent_alter = 0
-    count_recent_alter = 0
-    for row in csvobj:
-        len_recent_alter += 1
-        count_recent_alter += int(row['AlterCount'])
-    if len_recent_alter > 0:
-        return count_recent_alter / len_recent_alter
-    return -1
-
-
-def read_csv_in_folder(folder_path):
-    mean_alter_dict = {}
-    nb_ego_treated = 0
-    for file in os.listdir(folder_path):
-        filename = os.fsdecode(file)
-        path_to_file = os.path.join(folder_path, filename)
-        if filename.endswith(".csv"):
-            with open(path_to_file, newline='') as csvfile:
-                csvobj = csv.DictReader(csvfile)
-                id_ego = get_ego_id(filename)
-                mean_recent_alter = round(mean_recent_alter_ego(csvobj))
-                if mean_recent_alter != -1:
-                    add_value_to_dict(mean_alter_dict, mean_recent_alter, 1)
-    return mean_alter_dict
-
-
-def recent_alters_freq_perc_df(folder_path):
-    mean_alter_dict = read_csv_in_folder(folder_path)
-    df = pd.DataFrame(mean_alter_dict.items())
-    df = df.rename(columns={0: "Mean", 1: "Nbego"})
-    df['Percentage'] = df.Nbego.apply(lambda x: x/df['Nbego'].sum() * 100)
-    df.rename(columns={'Mean': 'Average_Recent_Ties',
-                       'Nbego': 'Frequency'}, inplace=True)
-    df.sort_values(by=['Average_Recent_Ties'], inplace=True)
-    return df
-
-
-def all_recent_alters_csv_to_df(folder_path):
-    df_dask = dd.read_csv(folder_path, include_path_column=True)
+def load_alters_count():
+    df_dask = dd.read_csv(egos_recent_alter_count_path,
+                          include_path_column=True)
     df_dask['egos'] = df_dask['path'].str.split('/').str[-1]
     df_dask['egos'] = df_dask['egos'].str.split('_').str[0]
     df_dask = df_dask.drop('path', axis=1)
     return df_dask.compute()
 
 
-def age_gender_profession_csv_to_df(file_path):
-    return dd.read_csv(file_path).compute()
+def load_egos_sociocat():
+    return dd.read_csv(egos_sociocat_path).compute()
 
 
-def merged_df(df1, df2):
-    df_merged = df2.merge(df1, on='egos')
+def sociocat_altercount_merged(alters_count, sociocat):
+    df_merged = sociocat.merge(alters_count, on='egos')
     df_merged['age_range'] = pd.cut(
         x=df_merged['age'], bins=[0, 18, 30, 59, 75, 100])
     return df_merged
 
 
-def median_age_binned(df_merged):
+def avg_alters_distribution(alters_count):
+    alters_count['MeanAlterCount'] = alters_count['AlterCount']
+    avg_df = alters_count.groupby('egos', as_index=False).agg({
+        'MeanAlterCount': 'mean'})
+    avg_df['RoundedMean'] = avg_df.MeanAlterCount.apply(lambda x: round(x))
+    avg_df = avg_df.groupby(
+        'RoundedMean', as_index=False).agg({'egos': 'count'})
+    avg_df.rename(columns={'MeanAlterCount': 'Average_Recent_Ties',
+                           'egos': 'Frequency'}, inplace=True)
+    avg_df['Percentage'] = avg_df.Frequency.apply(
+        lambda x: x/avg_df['Frequency'].sum() * 100)
+    return avg_df
+
+
+def median_alters_distribution(alters_count):
+    alters_count['MedianAlterCount'] = alters_count['AlterCount']
+    median_df = alters_count.groupby('egos', as_index=False).agg(
+        {'MedianAlterCount': 'median'})
+    median_df = median_df.groupby(
+        'MedianAlterCount', as_index=False).agg({'egos': 'count'})
+    median_df.rename(columns={'MedianAlterCount': 'Median_Recent_Ties',
+                              'egos': 'Frequency'}, inplace=True)
+    median_df['Percentage'] = median_df.Frequency.apply(
+        lambda x: x/median_df['Frequency'].sum() * 100)
+    return median_df
+
+
+def median_ego_bin_age_distribution(df_merged):
     df_median = df_merged[df_merged.age.notna()].groupby(
         ['egos', 'age_range'], as_index=False).AlterCount.median()
     df_median.rename(
@@ -88,7 +71,7 @@ def median_age_binned(df_merged):
     return df_median
 
 
-def mean_age_df_binned(df_merged):
+def mean_ego_bin_age_distribution(df_merged):
     df_mean = df_merged[df_merged.age.notna()].groupby(
         ['egos', 'age_range'], as_index=False).AlterCount.mean()
     df_mean.rename(columns={'AlterCount': 'Mean_Alter_Count'}, inplace=True)
@@ -99,7 +82,7 @@ def mean_age_df_binned(df_merged):
     return df_mean
 
 
-def mean_age_df(df_merged):
+def mean_ego_age_distribution(df_merged):
     df_mean = df_merged[df_merged.age.notna()]\
         .groupby(['egos', 'age'], as_index=False)\
         .AlterCount\
@@ -108,7 +91,7 @@ def mean_age_df(df_merged):
     return df_mean
 
 
-def median_age_df(df_merged):
+def median_ego_age_distribution(df_merged):
     df_median = df_merged[df_merged.age.notna()]\
         .groupby(['egos', 'age'], as_index=False)\
         .AlterCount\
@@ -118,7 +101,7 @@ def median_age_df(df_merged):
     return df_median
 
 
-def mean_gender_df(df_merged):
+def mean_ego_gender_distribution(df_merged):
     df_mean = df_merged[df_merged.gender.notna()].groupby(
         ['egos', 'gender'], as_index=False).AlterCount.mean()
     df_mean.rename(columns={'AlterCount': 'Mean_Alter_Count'}, inplace=True)
@@ -127,7 +110,7 @@ def mean_gender_df(df_merged):
     return df_mean
 
 
-def median_gender_df(df_merged):
+def median_ego_gender_distribution(df_merged):
     df_median = df_merged[df_merged.gender.notna()].groupby(
         ['egos', 'gender'], as_index=False).AlterCount.median()
     df_median.rename(
@@ -137,7 +120,7 @@ def median_gender_df(df_merged):
     return df_median
 
 
-def mean_professions(df_merged):
+def mean_ego_professions_distribution(df_merged):
     df_mean = df_merged[df_merged.profession.notna()].groupby(
         ['egos', 'profession'], as_index=False).AlterCount.mean()
     df_mean.rename(columns={'AlterCount': 'Mean_Alter_Count'}, inplace=True)
@@ -146,7 +129,7 @@ def mean_professions(df_merged):
     return df_mean
 
 
-def median_professions(df_merged):
+def median_ego_professions_distribution(df_merged):
     df_median = df_merged[df_merged.profession.notna()].groupby(
         ['egos', 'profession'], as_index=False).AlterCount.median()
     df_median.rename(
@@ -154,3 +137,13 @@ def median_professions(df_merged):
     df_median = df_median[df_median.Median_Alter_Count.notna()].groupby(['profession'], as_index=False)\
         .agg({'Median_Alter_Count': 'median', 'egos': 'count'})
     return df_median
+
+
+if __name__ == "__main__":
+    alters_count = load_alters_count()
+    sociocat = load_egos_sociocat()
+    altercount_sociocat = sociocat_altercount_merged(alters_count, sociocat)
+    avg_alters_per_ego = avg_alters_distribution(alters_count)
+    median_alters_per_ego = median_alters_distribution(alters_count)
+    print(avg_alters_per_ego)
+    print(median_alters_per_ego)
