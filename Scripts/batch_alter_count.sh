a1e07e10de6aaa4be8c17c7630cb7fb4868a923f
HyperionSuperNova <n.amari.pro@gmail.com>
2020-07-06 13:32:14 +0200

    batch

---
+++ Scripts/batch_alter_count.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/0 -o /home/nabil/results/Alter-count-csv-plots/0 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/1 -o /home/nabil/results/Alter-count-csv-plots/1 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/2 -o /home/nabil/results/Alter-count-csv-plots/2 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/3 -o /home/nabil/results/Alter-count-csv-plots/3 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/4 -o /home/nabil/results/Alter-count-csv-plots/4 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/5 -o /home/nabil/results/Alter-count-csv-plots/5 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/6 -o /home/nabil/results/Alter-count-csv-plots/6 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/7 -o /home/nabil/results/Alter-count-csv-plots/7 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/8 -o /home/nabil/results/Alter-count-csv-plots/8 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/9 -o /home/nabil/results/Alter-count-csv-plots/9 &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/a -o /home/nabil/results/Alter-count-csv-plots/a &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/b -o /home/nabil/results/Alter-count-csv-plots/b &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/c -o /home/nabil/results/Alter-count-csv-plots/c &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/d -o /home/nabil/results/Alter-count-csv-plots/d &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/e -o /home/nabil/results/Alter-count-csv-plots/e &
+ nohup python3 alters_count.py -d /home/nabil/results/Alter-cluster-timestamp-sorted/f -o /home/nabil/results/Alter-count-csv-plots/f &
