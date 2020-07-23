5339abe5274ec8565f766664abfcb307cf815440
HyperionSuperNova <n.amari.pro@gmail.com>
2020-07-23 15:52:12 +0200

    First draft of the script to analyse the approvale rates, need to check edge case

---
+++ Scripts/approved_friends_analysis.py
@@ -0,0 +1,88 @@
+import csv
+import os
+from pathlib import Path
+from datetime import *
+
+csv_path = Path('../Alter-count-csv-plots/')
+output_path = Path('../output/friends_approval_analysis.csv')
+
+
+def plus_one_year(dicoreader):
+    first_row = next(dicoreader)
+    tmp = next(dicoreader)
+    first_month, first_year = int(first_row['Month']), int(first_row['Year'])
+    tmp_month, tmp_year = int(tmp['Month']), int(tmp['Year'])
+    while tmp_month != first_month and tmp_year != (first_year + 1):
+        tmp = next(dicoreader)
+        tmp_month, tmp_year = int(tmp['Month']), int(tmp['Year'])
+    return tmp
+
+
+def diff_month(month1, month2, year1, year2):
+    return ((year2 - year1) * 12) + (month2 - month1)
+
+
+def get_last_day_of_month(datep):
+    next_month = datep.replace(day=28) + timedelta(days=4)
+    last_day_of_month = next_month - timedelta(days=next_month.day)
+    return datetime.strftime(last_day_of_month, '%d-%m-%Y')
+
+def mk_int(s):
+    s = s.strip()
+    return int(s) if s else 0
+
+def process_ego(ego_path, dicowriter):
+    id_ego = ego_path.name.split('_')[0]
+    dicoreader = csv.DictReader(ego_path.open('r', encoding='utf-8'))
+    try:
+        tmp_row = plus_one_year(dicoreader)
+        first_month, first_year = int(tmp_row['Month']), int(tmp_row['Year'])
+        tmp_month, tmp_year = int(tmp_row['Month']), int(tmp_row['Year'])
+        count_thres = 0
+        total_in_thres = 0
+        for row in dicoreader:
+            row_month, row_year = int(row['Month']), int(row['Year'])
+            if diff_month(tmp_month, row_month, tmp_year, row_year) == 1 and mk_int(row['approved_friends']) >= 20:
+                count_thres += 1
+                tmp_month, tmp_year = row_month, row_year
+                total_in_thres += int(row['approved_friends'])
+            else:
+                if count_thres >= 2:
+                    delta_month = diff_month(first_month, tmp_month, first_year, tmp_year)
+                    date_begin = get_last_day_of_month(datetime(first_year, first_month, 1))
+                    date_end = get_last_day_of_month(datetime(tmp_year, tmp_month, 1))
+                    dicowriter.writerow(
+                        {'id_ego': id_ego, 'date_begin': date_begin, 'date_end': date_end, 'month_diff': delta_month,
+                         'nb_total': total_in_thres, 'type': 'approvedfriends', 'thresold': 2, 'smoothing': 20})
+                count_thres = 0
+                total_in_thres = 0
+                first_month, first_year = int(row['Month']), int(row['Year'])
+                tmp_month, tmp_year = int(row['Month']), int(row['Year'])
+    except Exception as excp:
+        print(excp)
+
+
+def process_egos(filespath, dicowriter):
+    for file in filespath:
+        process_ego(file, dicowriter)
+
+
+def process_files():
+    files_path = csv_path.glob('*/csv/*.csv')
+    return files_path
+
+
+def get_csvwriter():
+    csvf = output_path.open('a', encoding='utf-8')
+    dicowriter = csv.DictWriter(
+        csvf, fieldnames=['id_ego', 'date_begin', 'date_end', 'month_diff', 'nb_total', 'type', 'thresold', 'smoothing'],
+        delimiter=',')
+    dicowriter.writeheader()
+    return dicowriter, csvf
+
+
+if __name__ == '__main__':
+    files_path = process_files()
+    dicowriter, file = get_csvwriter()
+    process_egos(files_path, dicowriter)
+    file.close()
